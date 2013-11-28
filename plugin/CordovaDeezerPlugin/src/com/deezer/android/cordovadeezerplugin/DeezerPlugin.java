c1a642743d234cb7aa2372e27344b00d572b5bfc
Xavier Gouchet <xg@deezer/com>
2013-11-28 14:05:21 +0100

    Initial plugin test

---
+++ plugin/CordovaDeezerPlugin/src/com/deezer/android/cordovadeezerplugin/DeezerPlugin.java
@@ -0,0 +1,39 @@
+package com.deezer.android.cordovadeezerplugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.widget.Toast;
+
+/**
+ * 
+ * @author Xavier
+ * 
+ */
+public class DeezerPlugin extends CordovaPlugin {
+
+	private CordovaInterface mInterface;
+	private CordovaWebView mWebView;
+
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
+
+		mInterface = cordova;
+		mWebView = webView;
+	}
+
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+
+		Toast.makeText(mInterface.getActivity(), action,
+				Toast.LENGTH_LONG).show();
+
+		return true;
+	}
+}
